b2300093b0f56b6b0344af24b98022a0ead2ba7e
elliottbache <87441242+elliottbache@users.noreply.github.com>
2021-07-15 16:15:18 +0200

    Update bank.sol
    Added comments.

--- examples/bank/bank.sol
+++ examples/bank/bank.sol
@@ -2,18 +2,21 @@ pragma solidity 0.7.5;
 import "./Ownable.sol";
 import "./Destroyable.sol";
 
+// Create a bank that can receive and send money, including a selfdestruct
 contract Bank is Ownable, Destroyable {
-    
+
     mapping(address => uint) balance;
     
     event depositDone(uint amount, address indexed depositedTo);
     
+    // Deposit money to bank from sender.  Creates event in log.
     function deposit() public payable returns (uint)  {
         balance[msg.sender] += msg.value;
         emit depositDone(msg.value, msg.sender);
         return balance[msg.sender];
     }
     
+    // Withdraw money from bank to sender.  Requires the sender's balance be greater than requested amount.  Creates event in log.
     function withdraw(uint amount) public returns (uint){
         require(balance[msg.sender] >= amount);
         balance[msg.sender] -= amount;
@@ -21,10 +24,12 @@ contract Bank is Ownable, Destroyable {
         return balance[msg.sender];
     }
     
+    // Return the balance for the sender's bank account
     function getBalance() public view returns (uint){
         return balance[msg.sender];
     }
     
+    // Transfer money from sender's bank account to another person's bank account.
     function transfer(address recipient, uint amount) public {
         require(balance[msg.sender] >= amount, "Balance not sufficient");
         require(msg.sender != recipient, "Don't transfer money to yourself");
@@ -36,9 +41,9 @@ contract Bank is Ownable, Destroyable {
         assert(balance[msg.sender] == previousSenderBalance - amount);
     }
     
+    // Updates balances for sender and receiver of bank transfer.
     function _transfer(address from, address to, uint amount) private {
         balance[from] -= amount;
         balance[to] += amount;
     }
-    
 }
