cb5d512b95dd489c6601f8f750ce14193dc51c96
elliottbache <87441242+elliottbache@users.noreply.github.com>
2021-07-15 16:16:44 +0200

    Update destroyable.sol
    Added comments.

--- examples/bank/destroyable.sol
+++ examples/bank/destroyable.sol
@@ -1,10 +1,11 @@
 import "./Ownable.sol";
+
 pragma solidity 0.7.5;
 
+// selfdestruct function that can only be called by owner.
 contract Destroyable is Ownable {
-
-  function destroy() public onlyOwner {
-    address payable receiver = msg.sender;
-    selfdestruct(receiver);
-  }
+    function destroy() public onlyOwner {
+        address payable receiver = msg.sender;
+        selfdestruct(receiver);
+    }
 }
